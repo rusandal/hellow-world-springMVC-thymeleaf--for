b8bcff48e975b5a558e197c6dd04fa32ea70aea9
sandal <ru-sandal@yandex.ru>
2022-03-22 20:20:48 +0300

    Hello world

--- src/main/java/ru/minikhanov/springMVC/MyController.java
+++ src/main/java/ru/minikhanov/springMVC/MyController.java
@@ -1,7 +1,9 @@
 package ru.minikhanov.springMVC;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -10,6 +12,11 @@ public class MyController {
     public String sayHello(){
         return "Hello!!!";
 
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname, Model model){
+        model.addAttribute("message", "Hello "+name+" "+ surname);
+        return "hello";
     }
 
 
